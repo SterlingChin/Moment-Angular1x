4d6da2ee8f01046d3bd03be70eb62d2d17f3cf54
Sterling Chin <sterling.chin@gmail.com>
2017-04-15 01:11:43 -0600

    trying to loop over forecast, not workin yet

--- JS/mainCtrl.js
+++ JS/mainCtrl.js
@@ -12,6 +12,36 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSvc, userPref
     ($scope.name === "enter name") ? true: false
   }
 
+  //  ┌─┐┬  ┌─┐┌─┐┬┌─
+  //  │  │  │ ││  ├┴┐
+  //  └─┘┴─┘└─┘└─┘┴ ┴
+
+  // let updateClock = ()=>{
+  //   let currentTime = new Date();
+  //   let currentHours = currentTime.getHours();
+  //   let currentMinutes = currentTime.getMinutes();
+  //   currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+  //   let timeOfDay = (currentHours < 12) ? "AM" : "PM";
+  //   currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+  //   currentHours = (currentHours === 0) ? 12 : currentHours;
+  //   let currentTimeString = currentHours + ":" + currentMinutes + " " + timeOfDay;
+  //   document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+  // }
+
+
+  let updateClock = $scope.time = moment().format('h:mm A')
+
+  $scope.todayDate = moment().format('MMM Do YYYY');
+
+  let day1 = moment().add(1, 'days').format('MMM Do');
+  let day2 = moment().add(2, 'days').format('MMM Do');
+  let day3 = moment().add(3, 'days').format('MMM Do');
+  let day4 = moment().add(4, 'days').format('MMM Do');
+  let day5 = moment().add(5, 'days').format('MMM Do');
+  let day6 = moment().add(6, 'days').format('MMM Do');
+  let calendar = [day1, day2, day3, day4, day5, day6]
+  console.log(calendar)
+
   //  ┌─┐ ┬ ┬┌─┐┌┬┐┌─┐┌─┐
   //  │─┼┐│ ││ │ │ ├┤ └─┐
   //  └─┘└└─┘└─┘ ┴ └─┘└─┘                          
@@ -30,27 +60,41 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSvc, userPref
     mainSvc.getWeather(zipcode).then(response => {
       $scope.weather = response;
     })
-    mainSvc.getForecast(zipcode).then(forecastObject => {
-      $scope.forecastTempHigh = forecastObject.tempHigh;
-      $scope.forecastTempLow = forecastObject.tempLow;
-      $scope.forecastTempHigh1 = forecastObject.tempHigh1;
-      $scope.forecastTempLow1 = forecastObject.tempLow1;
-      $scope.forecastDesc1 = forecastObject.desc1;
-      $scope.forecastTempHigh2 = forecastObject.tempHigh2;
-      $scope.forecastTempLow2 = forecastObject.tempLow2;
-      $scope.forecastDesc2 = forecastObject.desc2;
-      $scope.forecastTempHigh3 = forecastObject.tempHigh3;
-      $scope.forecastTempLow3 = forecastObject.tempLow3;
-      $scope.forecastDesc3 = forecastObject.desc3;
-      $scope.forecastTempHigh4 = forecastObject.tempHigh4;
-      $scope.forecastTempLow4 = forecastObject.tempLow4;
-      $scope.forecastDesc4 = forecastObject.desc4;
-      $scope.forecastTempHigh5 = forecastObject.tempHigh5;
-      $scope.forecastTempLow5 = forecastObject.tempLow5;
-      $scope.forecastDesc5 = forecastObject.desc5;
-      $scope.forecastTempHigh6 = forecastObject.tempHigh6;
-      $scope.forecastTempLow6 = forecastObject.tempLow6;
-      $scope.forecastDesc6 = forecastObject.desc6;
+    mainSvc.getForecast(zipcode).then(response => {
+      console.log(response)
+      $scope.weatherForecast = response
+      let test = response => {
+        console.log('1',response)
+        response.shift()
+        let forecast = response
+        console.log('2',forecast)
+        for (let i = 0; i < forecast.length; i++) {
+          forecast[i].day = calendar[i]
+        }
+        $scope.forecast = forecast
+      console.log($scope.forecast)
+    }
+    test(response)
+      // $scope.forecastTempHigh = forecastObject.tempHigh;
+      // $scope.forecastTempLow = forecastObject.tempLow;
+      // $scope.forecastTempHigh1 = forecastObject.tempHigh1;
+      // $scope.forecastTempLow1 = forecastObject.tempLow1;
+      // $scope.forecastDesc1 = forecastObject.desc1;
+      // $scope.forecastTempHigh2 = forecastObject.tempHigh2;
+      // $scope.forecastTempLow2 = forecastObject.tempLow2;
+      // $scope.forecastDesc2 = forecastObject.desc2;
+      // $scope.forecastTempHigh3 = forecastObject.tempHigh3;
+      // $scope.forecastTempLow3 = forecastObject.tempLow3;
+      // $scope.forecastDesc3 = forecastObject.desc3;
+      // $scope.forecastTempHigh4 = forecastObject.tempHigh4;
+      // $scope.forecastTempLow4 = forecastObject.tempLow4;
+      // $scope.forecastDesc4 = forecastObject.desc4;
+      // $scope.forecastTempHigh5 = forecastObject.tempHigh5;
+      // $scope.forecastTempLow5 = forecastObject.tempLow5;
+      // $scope.forecastDesc5 = forecastObject.desc5;
+      // $scope.forecastTempHigh6 = forecastObject.tempHigh6;
+      // $scope.forecastTempLow6 = forecastObject.tempLow6;
+      // $scope.forecastDesc6 = forecastObject.desc6;
     });
   }
   const getZip = () => {
@@ -82,30 +126,6 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSvc, userPref
   greeting();
 
 
-  //  ┌─┐┬  ┌─┐┌─┐┬┌─
-  //  │  │  │ ││  ├┴┐
-  //  └─┘┴─┘└─┘└─┘┴ ┴
-
-  let updateClock = ()=>{
-    let currentTime = new Date();
-    let currentHours = currentTime.getHours();
-    let currentMinutes = currentTime.getMinutes();
-    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-    let timeOfDay = (currentHours < 12) ? "AM" : "PM";
-    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
-    currentHours = (currentHours === 0) ? 12 : currentHours;
-    let currentTimeString = currentHours + ":" + currentMinutes + " " + timeOfDay;
-    document.getElementById("clock").firstChild.nodeValue = currentTimeString;
-  }
-
-  $scope.time = moment().format('h:mm A');
-
-  $scope.todayDate = moment().format('MMM Do YYYY');
-  $scope.day2 = moment().add(2, 'days').format('MMM Do');
-  $scope.day3 = moment().add(3, 'days').format('MMM Do');
-  $scope.day4 = moment().add(4, 'days').format('MMM Do');
-  $scope.day5 = moment().add(5, 'days').format('MMM Do');
-  $scope.day6 = moment().add(6, 'days').format('MMM Do');
 
 
   //  ┬ ┬┌─┐┌┬┐┌─┐┬ ┬┌─┐┌─┐
